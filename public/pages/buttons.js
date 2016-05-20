ae942e99c52b702cb0076940592421ae5fba80fd
Andrew Booker <andy989989@gmail.com>
2016-05-20 12:58:10 +1200

    fixed syntax

--- public/pages/buttons.js
+++ public/pages/buttons.js
@@ -5,7 +5,7 @@
 
 	var ipAddr = 'https://morning-dawn-49717.herokuapp.com';
 	//var ipAddr = 'localhost:8080';
-	var data = {'UserName':'Andy','password':'test1'};
+	var data = {'userName':'Andy','password':'test1'};
 
 	console.log("test");
 
