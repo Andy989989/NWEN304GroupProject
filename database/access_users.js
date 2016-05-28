4a8c9686e6dd72545f3d15cd93fc8a87032c8db4
Ben <bw1petra@gmail.com>
2016-05-28 13:37:59 +1200

    added helpful commenting so I remember what each method does in the future, and started on some admin database manipulation functions

--- database/access_users.js
+++ database/access_users.js
@@ -11,46 +11,90 @@ client.connect();
 app.use(bp.urlencoded({extended:true}));
 app.use(bp.json());
 
-function postData(name, password){
-	client.query("update users set password='"+password+"' where name='"+name+"'");
-}
-
+/* Returns all occurrences (should only be one, or zero) of a given name in the database.
+ * If the query is successful (ie. the user does exist in the database) then this method
+ * returns the password that is associated with that name, otherwise it returns an error
+ * message, starting with 'ERROR: ', and followed by a short sentence describing the nature
+ * of the error.
+ */
 exports.get = function(name){
 	var name = req.body.name;
 	if(name == undefined || name == null){
 		return "ERROR: Missing value for name.";
 	}
 	var password = client.query("select * from users where name='"+name+"'", function(err){
-		return "ERROR: that name does not exist in the database.";
+		if(err){ //User does not exist and therefore has no password to get.
+			return "ERROR: that name does not exist in the database.";
+		}
 	});
 	return password;
 }
 
+/* Adds a user to the database. If the adding is successful (ie. the given name and password
+ * were valid and no errors were thrown by the database (that is, the user did not already
+ * exist)) then this method returns a 'Success.' message, otherwise it returns an error
+ * message, starting with 'ERROR: ', and followed by a short sentence describing the nature
+ * of the error.
+ */
 exports.put = function(name, password){
 	var missing = check_everything_is_here(name, password);
-	if(missing == null){
+	if(missing == null){ //Name and password are valid.
 		client.query("insert into users (name, password) values ('"+name+"','"+password+"')", function(err){
-			if(err){
+			if(err){ //There is already a user with that name in the database.
 				return "ERROR: User already exists.";
 			}
 		});
 		return "Success.";
 	}
-	return "ERROR: Missing a valid " + missing;
+	return missing;
 }
 
+/* Updates the password associated with a user's name. If the update is successful (ie. the
+ * given name and password were valid and no errors were thrown by the database) then this
+ * method returns a 'Success.' message, otherwise it returns an error message, starting with
+ * 'ERROR: ', and followed by a short sentence describing the nature of the error.
+ */
+exports.update_password = function(name, new_password){
+	var missing = check_everything_is_here(name, new_password);
+	if(missing != null){
+		return missing;
+	}
+	client.query("update users set password='"+new_password+"' where name='"+name+"'", function(err){
+		if(err){ //User does not exist, and therefore has no password to update.
+			return "ERROR: that name is not in the database.";
+		}
+	});
+	return "Success."
+}
+
+/*
+ * =====================================================
+ * HELPER METHODS
+ * =====================================================
+ */
+
+/* Ensures that the given name and password are valid (ie. neither are null or undefined,
+ * and the name contains only letters, numbers, and underscores (this prevents SQL
+ * injections)). This returns an error message of the form 'ERROR: ' followed by a short
+ * description of the error, if one is thrown. Otherwise, it returns null to signal success.
+ */
 function check_everything_is_here(name, password){
 	//Check name exists and is valid
 	if(name == undefined || name == null || !(ensure_only_letters_and_numbers(name))){
-		return "name";
+		return "ERROR: Missing a valid name.";
 	}
 	//Check password exists and is valid
 	if(password == null || password == undefined){
-		return "password";
+		return "ERROR: Missing a valid password.";
 	}
+	// #winning
 	return null;
 }
 
+/* This methods checks whether a given word contains anything that is not a letter, number,
+ * or underscore, returning a boolean of whether it is valid (does not contain anything else)
+ * or not. This is useful for preventing SQL injection attacks on the database.
+ */
 function ensure_only_letters_and_numbers(word){
 	return /^\w+$/.test(word);
 }
