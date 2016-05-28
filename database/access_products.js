0e32cae29b7570dfad6d9f27a13c039c1b12209d
Gareth Tucker <Gareth Tucker>
2016-05-28 13:50:17 +1200

    added display page

--- database/access_products.js
+++ database/access_products.js
@@ -92,7 +92,7 @@ function handle_query(query, res){
 			query_results.push(JSON.stringify(row));
 			});
 	query.on('end', function(){
-				res.render('Display', {results: query_results})
+				res.render('display', {results: query_results})
 				//res.json(query_results);
 			});
 }
