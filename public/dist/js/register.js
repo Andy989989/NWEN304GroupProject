e8eedbc256c7bc5bd68e19f591e6783d56d1bd02
Andrew Booker <andy989989@gmail.com>
2016-05-30 15:46:49 +1200

    fixed all of the merge errors

---
+++ public/dist/js/register.js
@@ -0,0 +1,65 @@
+
+
+
+	//var ipAddr = 'https://morning-dawn-49717.herokuapp.com/';
+	var ipAddr = 'http://130.195.4.151:8080';
+
+	console.log("test");
+
+	
+	//<input type="text" class="form-control" id="registerUsername" rows="5" placeholder="Username">
+	//<input type="text" class="form-control" id="registerPassword" rows="5" placeholder="Password">
+	//<input type="text" class="form-control" id="registerConfirmPassword" rows="5" placeholder="Confirm Password">
+
+
+
+$(document).ready(function(e) {
+
+
+
+	$('#submit').button().click(
+
+		function() {
+			console.log($('#registerUsername').val());
+			console.log($('#registerPassword').val());
+			console.log($('#registerConfirmPassword').val());
+			var userName = $('#registerUsername').val();
+			var password = $('#registerPassword').val();
+
+
+
+			if(userName == null || userName == undefined || userName == ''){
+				alert("please enter a user Name and password");
+			}else{
+				var data = {'userName':userName,'password':password};
+				console.log(data);
+				$.ajax({	
+					method:'POST',
+					url: ipAddr+"/newUser",
+					data: data,
+    				//url: ipAddr+"/test",
+    				error: function(data,status) {
+    					alert("failed Register: " + status);
+    					console.log(data);
+    				},
+    				success: function(data,status){
+    					alert("succesful Register: " + status);
+    					console.log(data);
+    			}
+
+    			});
+
+			}
+
+			
+
+			
+
+
+
+
+		});
+
+
+});
+
