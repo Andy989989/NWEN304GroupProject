4b2306af1266303b39f9f35cb0e548f88b94c210
Andy989989 <andy989989@gmail.com>
2016-06-07 18:53:56 +1200

    fixed address errors

--- public/dist/js/login.js
+++ public/dist/js/login.js
@@ -1,11 +1,10 @@
-var ipAddr = 'https://morning-dawn-49717.herokuapp.com';
-//var ipAddr = 'st:8080';
+'https://127.0.0.1:8080'//var ipAddr = 'st:8080';
 //var data = {'userName':'Andy','password':'test1'};
 console.log("test");
 
 $(document).ready(function(e) {
 //var ipAddr = 'https://morning-dawn-49717.herokuapp.com';
-    var ipAddr = 'http://130.195.4.164:8080';
+    //var ipAddr = 'http://130.195.4.164:8080';
     //var data = {'userName':'Andy','password':'test1'};
     var token;
     $('#submitLogin').button().click(
