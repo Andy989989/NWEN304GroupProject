7673a50c0a44cbbd5a93317542f7092633c7d064
Ben <bw1petra@gmail.com>
2016-06-19 01:46:05 +1200

    I think I finally have the right return

--- database/access_users.js
+++ database/access_users.js
@@ -139,7 +139,7 @@ function get_suggestion_based_on_weather(geo, suggestions, callback){
 	console.log("entering weather");
 	yahoo_weather.getFullWeather('denver,co').then(function(res){
 		console.log("in yahoo weather");
-		callback(res.query.results);
+		callback(res.query.results.channel.item.condition.text);
 		console.log("done yahoo weather");
 	});
 	return;
