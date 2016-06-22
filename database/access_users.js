e804bf661a025012563f8c67b073210a71af7ae1
Ben <bw1petra@gmail.com>
2016-06-22 15:07:14 +1200

    returned nicelyer from that case

--- database/access_users.js
+++ database/access_users.js
@@ -32,7 +32,7 @@ exports.get = function(name, res, callback){
 			return err;
 			}
 			if(rows.rows[0] == undefined){
-				return err;
+				res.status(400).send("Sorry, you aren't in our database. Please Register first, and then try again");
 			}
 			callback(res, rows.rows[0].password);
 			});
