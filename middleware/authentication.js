4fba0ed297059485d20204d648002028fba73b4d
Ben <bw1petra@gmail.com>
2016-06-26 16:14:35 +1200

    process.nextTick

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -12,11 +12,11 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 
 
 /*
-var pg = require('pg').native;
-var connectionString; //= "postgres://watsonben:mypassword@depot:5432/watsonben_nodejs"; //TODO Create a new database.
-var client = new pg.Client(connectionString);
-client.connect();
-*/
+   var pg = require('pg').native;
+   var connectionString; //= "postgres://watsonben:mypassword@depot:5432/watsonben_nodejs"; //TODO Create a new database.
+   var client = new pg.Client(connectionString);
+   client.connect();
+ */
 
 var secret ='secretKeyThing'
 var exports = module.exports = {};
@@ -24,9 +24,9 @@ var database = [{'userName':'Andy','password':'test1'}];
 
 exports.testAuth = function(req,res){
 
-//console.log(req.user.access_token);
-var data={'text':'got into testAuth : authentication succesfull','token':req.user.access_token };
-res.send(data);
+	//console.log(req.user.access_token);
+	var data={'text':'got into testAuth : authentication succesfull','token':req.user.access_token };
+	res.send(data);
 
 }
 
@@ -35,10 +35,10 @@ exports.authenticate = function (req, res,next){
 
 	if (req.isAuthenticated()){
 		console.log("authenticated");
-    	return next();
-  	}else{
-	 return res.status(401).send("Failed to authenticate: please login");
-}
+		return next();
+	}else{
+		return res.status(401).send("Failed to authenticate: please login");
+	}
 
 }
 
@@ -46,47 +46,48 @@ exports.authenticate = function (req, res,next){
 
 
 exports.newUser = function(req,res,next){
-console.log(req.body);
-console.log("Creating a new User");
-/*
-assuming this data is being sent from the client
-{userName: "test",password:"test"}
-*/
-//var query = client.query('SELECT * from logins where userName = $1', [req.body.userName]);
+	console.log(req.body);
+	console.log("Creating a new User");
+	/*
+	   assuming this data is being sent from the client
+	   {userName: "test",password:"test"}
+	 */
+	//var query = client.query('SELECT * from logins where userName = $1', [req.body.userName]);
 
-// error testing
-if(!req.body.hasOwnProperty('username') || !req.body.hasOwnProperty('password') ){
-	res.statusCode = 400;
-	return res.send('Error 400');
-}
-var name = req.body.username;
-var pass = req.body.password;
-
-
-var hash = bcrypt.hashSync(pass, salt);
-console.log("Hashed password"+ hash);
-
-
-
-// TODO  put the data into the databse here
-//var data = {"userName":name,"password":hash};
-//database.push(data);
-var user = users.put(name,hash);
-
-console.log("added the data to the database:"+name+":"+pass);
-console.log(user);
-console.log("+++++++++++++++++");
-if(user!=undefined){
-	console.log(user);
-	if(user[0].severity == 'ERROR'){
-		// if this equals -1 that means there is no error
-		// could change to get the currentn value of errror.
-
-		// 409 - duplicate data
-		console.log("There was a problem");
-		res.status(409).send("User Already exsists in the database");
+	// error testing
+	if(!req.body.hasOwnProperty('username') || !req.body.hasOwnProperty('password') ){
+		res.statusCode = 400;
+		return res.send('Error 400');
 	}
-}
-//console.log(data);
-res.send('user created');
+	var name = req.body.username;
+	var pass = req.body.password;
+
+
+	var hash = bcrypt.hashSync(pass, salt);
+	console.log("Hashed password"+ hash);
+
+
+
+	// TODO  put the data into the databse here
+	//var data = {"userName":name,"password":hash};
+	//database.push(data);
+	var user = users.put(name,hash);
+	process.nextTick(function(){
+			console.log("added the data to the database:"+name+":"+pass);
+			console.log(user);
+			console.log("+++++++++++++++++");
+			if(user!=undefined){
+			console.log(user);
+			if(user[0].severity == 'ERROR'){
+			// if this equals -1 that means there is no error
+			// could change to get the currentn value of errror.
+
+			// 409 - duplicate data
+			console.log("There was a problem");
+			res.status(409).send("User Already exsists in the database");
+			}
+			}
+			//console.log(data);
+			res.send('user created');
+			});
 }
