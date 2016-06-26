611fc03ff235657fe239ffb226e732ef7c9c6ab9
Andrew Booker <andy989989@gmail.com>
2016-06-26 16:48:54 +1200

    eliminated console logs

--- middleware/Config.js
+++ middleware/Config.js
@@ -1,7 +1,6 @@
 // load all the things we need
 var LocalStrategy   = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
-
 var users = require('../database/access_users.js');
 var bcrypt = require('bcrypt');
 var salt = bcrypt.genSaltSync(10);
@@ -9,16 +8,12 @@ var salt = bcrypt.genSaltSync(10);
 
 
 module.exports = function(passport) {
-
 passport.serializeUser(function(user, cb) {
   cb(null, user);
 });
-
 passport.deserializeUser(function(obj, cb) {
   cb(null, obj);
 });
-
-
 //=====================================
 //AUTHENTICATION SETUP
 //=====================================
@@ -26,31 +21,10 @@ passport.use('facebook',new FacebookStrategy({
     clientID: 261460150870678,//process.env.CLIENT_ID,
     clientSecret: '54da0a9f6352a8adf21c359a545b2257',//process.env.CLIENT_SECRET,
     callbackURL: 'https://morning-dawn-49717.herokuapp.com/login/facebook/return'
-   //passReqToCallback : true,
-
-
   },
   function(accessToken, refreshToken, profile, cb) {
-
-  	// need to check if fb data is in the data base. if not add it
-
- 	//  	//var user = users.put(name,hash);
-	// console.log(" FB data to the database:"+name+":"+pass);
-
-	// if(user!=undefined){
-	// var errorCheck = user.search("ERROR:"); 
-	// if(errorCheck != -1){
-	// // if this equals -1 that means there is no error
-	// // could change to get the currentn value of errror.
-
-	// // 409 - duplicate data
-	// console.log("There was a problem");
-	// res.status(409).send("User Already exsists in the database");
-	// }
-	//}
-	console.log(profile.id);
-	console.log(profile.displayName);
-
+	//console.log(profile.id);
+	//console.log(profile.displayName);
     return cb(null, profile);
   }));
 
@@ -60,19 +34,9 @@ passport.use('local', new LocalStrategy({
      password : 'password',
 	passReqToCallBack : true
 }, function(username, password, done) {
-	console.log("getting into the local start");
-
 	process.nextTick(function() {
-
-		console.log(password);
-		console.log(username);
-
 		var databasePassword = users.get(username,null,function(res,returnedPassword){
-			console.log("data returned from database: " + returnedPassword);
-			//console.log(req.body.password);
-			console.log(password.toString());
 			if(bcrypt.compareSync(password, returnedPassword)){
-				console.log("getting in hash test");
 				return done(null, username);
 			}else{
 				return done(null, false, {message: "Message: username not found in database"});
@@ -84,13 +48,7 @@ passport.use('local', new LocalStrategy({
 		if(databasePassword == "ERROR: Missing valid value for name."){
 			return done(null, false, {message: "Message: username not found in database"});			
 		}
-
-		//return done(null, false, {message: "Message: username not found in database"});
-		//console.log("database lookup failed : "+databasePassword);
-
-	});			//res.status(404).send("Error when checking password");
-		//console.log(hash);
-		//console.log(returnedPassword);
+	});			
 }));
 
 }
--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,93 +1,42 @@
-var jwt = require('jsonwebtoken');
 var express = require('express');
 var users = require('../database/access_users.js');
 var bcrypt = require('bcrypt');
 var salt = bcrypt.genSaltSync(10);
-// load all the things we need
-var LocalStrategy   = require('passport-local').Strategy;
-var FacebookStrategy = require('passport-facebook').Strategy;
-
-
-
-
-
-/*
-   var pg = require('pg').native;
-   var connectionString; //= "postgres://watsonben:mypassword@depot:5432/watsonben_nodejs"; //TODO Create a new database.
-   var client = new pg.Client(connectionString);
-   client.connect();
- */
-
 var secret ='secretKeyThing'
 var exports = module.exports = {};
-var database = [{'userName':'Andy','password':'test1'}];
 
 exports.testAuth = function(req,res){
-
-	//console.log(req.user.access_token);
 	var data={'text':'got into testAuth : authentication succesfull','token':req.user.access_token };
 	res.send(data);
-
 }
 
 exports.authenticate = function (req, res,next){
-	console.log("gets into auth");
-
 	if (req.isAuthenticated()){
-		console.log("authenticated");
 		return next();
 	}else{
 		return res.status(401).send("Failed to authenticate: please login");
 	}
-
 }
 
-
-
-
 exports.newUser = function(req,res,next){
-	console.log(req.body);
-	console.log("Creating a new User");
-	/*
-	   assuming this data is being sent from the client
-	   {userName: "test",password:"test"}
-	 */
-	//var query = client.query('SELECT * from logins where userName = $1', [req.body.userName]);
 
-	// error testing
 	if(!req.body.hasOwnProperty('username') || !req.body.hasOwnProperty('password') ){
 		res.statusCode = 400;
 		return res.send('Error 400');
 	}
 	var name = req.body.username;
 	var pass = req.body.password;
-
-
 	var hash = bcrypt.hashSync(pass, salt);
 	console.log("Hashed password"+ hash);
 
-
-
-	// TODO  put the data into the databse here
-	//var data = {"userName":name,"password":hash};
-	//database.push(data);
 	var user = users.put(name,hash);
 	process.nextTick(function(){
-			console.log("added the data to the database:"+name+":"+pass);
-			console.log(user);
-			console.log("+++++++++++++++++");
-			if(user!=undefined){
+		if(user!=undefined){
 			console.log(user);
 			if(user[0].severity == 'ERROR'){
-			// if this equals -1 that means there is no error
-			// could change to get the currentn value of errror.
-
-			// 409 - duplicate data
-			console.log("There was a problem");
-			res.status(409).send("User Already exsists in the database");
-			}
-			}
-			//console.log(data);
+				res.status(409).send("User Already exsists in the database");
+		}
+	}
 			res.send('user created');
-			});
+		});
 }
